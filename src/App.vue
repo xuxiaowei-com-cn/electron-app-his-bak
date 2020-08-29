e2bb2b9ab132e174bc0f8dda34fe1e70c894f8f0
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-08-29 11:13:32 +0800

    删除 ffi-napi

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,28 @@
 <template>
-    <div id="app">
-        <img alt="Vue logo" src="assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js App"/>
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-    import HelloWorld from './components/HelloWorld.vue'
-    const { ipcRenderer } = require('electron');
+import HelloWorld from './components/HelloWorld.vue'
 
-    export default {
-        name: 'App',
-        components: {
-            HelloWorld
-        },
-        created() {
-            ipcRenderer.send('calculation', ['sum', 223, 443]);
-            ipcRenderer.on('reply-calculation', (event, arg) => {
-                console.log(arg)
-            });
-        }
-    }
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
 <style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
