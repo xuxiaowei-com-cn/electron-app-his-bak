baddae19cefcf9e4663b9934a301b7aa6a481ed6
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-08-29 10:17:16 +0800

    窗口 logo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 
+const path = require('path');
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
@@ -10,6 +12,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, 'assets/img/icon.png'),
     webPreferences: {
       nodeIntegration: true,
     },
